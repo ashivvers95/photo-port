b1ed3399d3c72d68e393f2025405094cb97dc768
Shivvers <ashivvers95@gmail.com>
2021-06-18 23:03:20 -0700

    created error messages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,40 @@
-import React from "react";
 import React, { useState } from "react";
+import {validateEmail} from '../../utils/helpers'
 
-const { name, email, message } = formState;
+
 
 function ContactForm() {
     const [ formState, setFormState ] = useState({name: '', email: '', message:''});
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(e) {
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+              } else {
+                setErrorMessage('');
+              }
+        } else { 
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name}is required.`)
+            }else {
+                setErrorMessage(' ');
+            }
+        }
+        if(!errorMessage) {
         setFormState({...formState, [e.target.name]: e.target.value })
     }
+    }
+
     function handleSubmit(e){
         e.preventDefault();
         console.log(formState);
     }
+
  return(
      <section>
          <h1>Contact me</h1>
